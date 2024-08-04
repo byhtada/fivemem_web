6916a33adc0b84b196da2f9fc0f2855149d7ec84
BNK <byhtada@gmail.com>
2024-08-04 13:11:50 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let api_url = "https://api.margiis-assistant.am/";
+
+if (window.location.href.includes("localhost")) {
+  api_url = "http://127.0.0.1:3000/";
+}
+
+document.getElementById('app').innerText = "HHHH"
+
